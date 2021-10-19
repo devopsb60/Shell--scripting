c62e13fc1cc652de9cb5eaf7d214fed9b4567ccf
cvreddy <chalamalavenkateshreddy@gmail.com>
2021-10-19 21:28:26 +0530

    start with Shell scripting

---
+++ 01-Shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# #! is called she bang  it denotes the path of the interpreter and also ensure with that interpretuer if you execute
+
+# any lines starts with # are comments and those are ignored during the executions.
+
+ls
+ls -a
+ls -l
+ls -A
