3cc4e84f7f7be920a7a7bd455a28e1cd562fd872
cvreddy <chalamalavenkateshreddy@gmail.com>
2021-10-20 21:11:35 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	02-print.sh

---
+++ 03-variables.sh
@@ -0,0 +1,18 @@
+#!\bin\bash
+
+## if we assign a name to a set of date, then that is a variable
+
+#Number
+
+a=1000
+
+#number
+
+b=cvreddy
+
+## In bash shell by default there is no default variables everything is considered as string
+
+#how to access the data in shell using $ character prefixing the variable name
+
+echo $a
+echo $b
