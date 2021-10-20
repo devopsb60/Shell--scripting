4752692f812b046ee63405ccb03d789d7170c7be
cvreddy <chalamalavenkateshreddy@gmail.com>
2021-10-20 20:16:06 +0530

    start with Shell scripting print command

---
+++ 02-print.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## To print some info we can use echo command or  printf
+
+## we chose to go with echo because of its syntaxing
+
+##syntax
+# to print
+
+echo Hello World
+echo welcome
+
+
