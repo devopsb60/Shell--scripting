2fd9efabdaafd2800d8db7d7458e70e1c2596b15
cvreddy <chalamalavenkateshreddy@gmail.com>
2021-10-21 21:45:22 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	02-print.sh

--- 04-read.sh
+++ 04-read.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p `enter your name:` name
-read -p `enter your age:` age
+read -p `enter your name: ` name
+read -p `enter your age: ` age
 
 echo -e `your name =${name} \n your age =${age}`
