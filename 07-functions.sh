efa8c46367f89d3b0843ffc9e84d054e15cac486
cvreddy <chalamalavenkateshreddy@gmail.com>
2021-10-22 22:14:24 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	02-print.sh

---
+++ 07-functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##functions should be always declared before used same like variable
+##so that is the reason functions we we find starting of the scripts
+
+funstions abc(){
+  echo i am a function abc
+  
+}
+
