5913bd2cbe723a930753835314058eec438aad45
cvreddy <chalamalavenkateshreddy@gmail.com>
2021-10-22 15:32:18 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	02-print.sh

--- 05-sp-vars.sh
+++ 05-sp-vars.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+#0indicated script name
 echo 0 = $0
+first argument to script
 echo 1 = $1
+#second argument
 echo 2 = $2
 
+#* abd # provides all the aruguments passed
 echo "* = $*"
 echo "# = $#"
+# @ provides the no of values  of arguments passed
 echo "@ = $@"
+
+echo -e "your name = $1\n your age =$2"
