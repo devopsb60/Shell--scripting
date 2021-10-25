aeb3460574595859fd2526960812ea3675af8053
cvreddy <chalamala03@gmail.com>
2021-10-25 15:43:15 +0530

    Simpel If command

--- 08-if.sh
+++ 08-if.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-read -p "enter username " Name
+read -p "enter username: " Name
 
 if ["#Name" == "root"]:
 then
   echo "hey your are root user"
+  else
+    echo "its not root user"
+  fi
