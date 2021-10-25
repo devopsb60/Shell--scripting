4ac10328b17eca9107d1b4672dd06f1f26acca5f
cvreddy <chalamala03@gmail.com>
2021-10-25 19:03:58 +0530

    created a Mongo db file

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source components/mongodb.sh
+source components/common.sh
 ##Setup MongoDB repos.
 print "download repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
