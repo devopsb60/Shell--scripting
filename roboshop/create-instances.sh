72d8bb723a800077578c43c653b63e7465d3bd47
cvreddy <chalamala03@gmail.com>
2021-10-26 21:43:50 +0530

    catalogue setup with id

--- roboshop/create-instances.sh
+++ roboshop/create-instances.sh
@@ -15,4 +15,4 @@ IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Rese
 ##aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | wc -l
 ##Xargs to remove quotes
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-aws route53 change-resourse-record-sets --hosted-zone-id Z01891252J9BRJM6PTJ64 --change-batch file:///tmp/record.json | jq
+aws route53 change-resource-record-sets --hosted-zone-id Z01891252J9BRJM6PTJ64 --change-batch file:///tmp/record.json | jq
