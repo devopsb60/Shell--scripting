c253032d1bfb21a0211df70e16e5d9881f68eb0c
cvreddy <chalamala03@gmail.com>
2021-10-26 14:06:45 +0530

    creating instance

--- create-instances.sh
+++ create-instances.sh
@@ -1,6 +1,8 @@
 #!/bin/usr
 
-count=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].InstanceId" | wc -l
+##aws configure
+
+count=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l
 )
   if [ $count -eq 0 ];then
   aws ec2 run-instances --image-id  ami-02e136e904f3da870 --instance-type t2.micro --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
@@ -9,3 +11,5 @@ count=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".R
     echo "instance already exits"
     fi
 
+
+##aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | wc -l
