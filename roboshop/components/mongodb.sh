a3eb85929f598832831fc453f7fde8736d160d70
cvreddy <chalamala03@gmail.com>
2021-10-26 15:01:02 +0530

    Mongo DB setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -9,13 +9,7 @@ Print "Install MongoDB"
 yum install -y mongodb-org  &>>$Log
 stat $?
 
-Print "enable mongoDB"
-systemctl enable mongod  &>>$Log
-stat $?
 
-Print "Start MongoDB"
-systemctl start mongod  &>>$Log
-stat $?
 
 ##Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
 ## sed -i "edit the file directly"-e /s/root/Root/g;- edit
@@ -23,18 +17,36 @@ stat $?
 ## sed -i -e '/root/d' file
 # sed -i -e '1 a hellw ' file
 #sed -i -e /etc/mongod
+
+Print"Update the mongod.conf"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/momgod.conf &>>$Log
 ##Config file: /etc/mongod.conf
+stat $?
 
-##then restart the service
+Print "then restart the service"
+systemctl restart mongod
+stat $?
 
-##systemctl restart mongod
+Print "enable mongoDB"
+systemctl enable mongod  &>>$Log
+stat $?
+
+
 ##Every Database needs the schema to be loaded for the application to work.
 ##Download the schema and load it.
 
-##curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+Print"donwloading schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$Log
+stat $?
 
-##cd /tmp
-##unzip mongodb.zip
-##cd mongodb-main
-##mongo < catalogue.js
-##mongo < users.js
+Print"unzip the schema"
+cd /tmp
+unzip -o mongodb.zip
+stat $?
+
+Print"Load Schema"
+cd mongodb-main
+mongo < catalogue.js &>>$Log
+mongo < users.js &>>$Log
+stat $?
+
