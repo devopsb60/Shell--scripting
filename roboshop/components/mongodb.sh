23029ac4a96476cff3bafc5e72249170743ad23a
cvreddy <chalamala03@gmail.com>
2021-10-26 14:43:10 +0530

    Mongo DB setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -5,10 +5,24 @@ print "download repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 stat $?
 ##Install Mongo & Start Service.
-##yum install -y mongodb-org
-##systemctl enable mongod
-##systemctl start mongod
+print "Install MongoDB"
+yum install -y mongodb-org  &>>$Log
+stat $?
+
+print "enable mongoDB"
+systemctl enable mongod  &>>$Log
+Stat $?
+
+print "Start MongoDB"
+systemctl start mongod  &>>$Log
+Stat $?
+
 ##Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+## sed -i "edit the file directly"-e /s/root/Root/g;- edit
+## sed -i -e '1 d' file name - to delate particular line
+## sed -i -e '/root/d' file
+# sed -i -e '1 a hellw ' file
+#sed -i -e /etc/mongod
 ##Config file: /etc/mongod.conf
 
 ##then restart the service
