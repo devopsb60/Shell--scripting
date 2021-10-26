6c5f1be4edb11b68dd96a2abf568d490cd2e128a
cvreddy <chalamala03@gmail.com>
2021-10-26 14:10:46 +0530

    creating instance

--- create-instances.sh
+++ create-instances.sh
@@ -3,7 +3,7 @@
 ##aws configure
 
 count=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
-)
+
   if [ $count -eq 0 ];then
   aws ec2 run-instances --image-id  ami-02e136e904f3da870 --instance-type t2.micr--tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
   echo "instance created"
